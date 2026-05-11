4c340400c36f1284ec289afe84617e5983f682b9
mcaramba563 <mcaramba563@gmail.com>
2026-05-11 11:03:53 +0300

    feat(CMakeLists): add cmake configaration module

--- include/List.hpp
+++ include/List.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdexcept>
 #include <exception>
 #include <memory>
---
+++ include/MySTL.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Function.hpp"
+#include "List.hpp"
+#include "SharedPtr.hpp"
+#include "Tuple.hpp"
+#include "UnorderedMap.hpp"
+#include "Variant.hpp"
